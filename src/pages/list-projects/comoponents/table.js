6a40b3bae35f40c775d879a4e3ef45fb23128792
Rahul Raj <rahulraj@Rahuls-MacBook-Pro.local>
2024-08-23 19:13:04 +0530

    update api url

--- src/pages/list-projects/comoponents/table.js
+++ src/pages/list-projects/comoponents/table.js
@@ -4,15 +4,20 @@ const Table = ({ headers = [], data = [] }) => {
       <thead className="">
         <tr className="bg-gray-100 text-gray-600 uppercase text-sm leading-norma">
           {headers.map(({ label }) => (
-            <th className="py-3 px-6 text-left">{label}</th>
+            <th className="py-3 px-6 text-left" key={label}>
+              {label}
+            </th>
           ))}
         </tr>
       </thead>
       <tbody className="text-gray-700 text-sm font-light">
-        {data.map((datum) => (
-          <tr className="border-b border-gray-200 hover:bg-gray-100">
-            {headers.map(({ getVal, key }) => (
-              <td className="py-3 px-6 text-left">
+        {data.map((datum, index) => (
+          <tr
+            className="border-b border-gray-200 hover:bg-gray-100"
+            key={index}
+          >
+            {headers.map(({ getVal, key, label }) => (
+              <td className="py-3 px-6 text-left key" key={label}>
                 {getVal ? getVal(datum) : datum[key]}
               </td>
             ))}
--- src/pages/list-projects/index.js
+++ src/pages/list-projects/index.js
@@ -1,22 +1,23 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Table from "./comoponents/table";
-const tempData = [
-  {
-    project: {
-      id: 1,
-      name: "New Website",
-      image:
-        "https://assets.amberstudent.com/inventories/1208941/fca4f2c9.jpeg",
-    },
-    progress: 75,
-    options: {
-      role: "value1",
-      badge: ["quick_learner", "top_performer"],
-    },
-  },
-];
+// const tempData = [
+//   {
+//     project: {
+//       id: 1,
+//       name: "New Website",
+//       image:
+//         "https://assets.amberstudent.com/inventories/1208941/fca4f2c9.jpeg",
+//     },
+//     progress: 75,
+//     options: {
+//       role: "value1",
+//       badge: ["quick_learner", "top_performer"],
+//     },
+//   },
+// ];
+const BASE_URL = "https://cartedo-user-app-70914f3279fb.herokuapp.com";
 export default function Home() {
-  const [data, setData] = useState(tempData);
+  const [data, setData] = useState([]);
 
   const headers = [
     {
@@ -36,6 +37,13 @@ export default function Home() {
       getVal: ({ options }) => <div>{options.badge?.join(",")}</div>,
     },
   ];
+
+  useEffect(() => {
+    fetch(`${BASE_URL}/users/1/projects`)
+      .then((res) => res.json())
+      .then((res) => setData(res));
+  }, []);
+
   return (
     <main>
       <Table data={data} headers={headers} />
